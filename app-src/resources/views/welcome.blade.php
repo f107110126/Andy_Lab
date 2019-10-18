938ff3b4f95b9ad0386ea37bb9a4a58f4d81a610
Andy Chen <f107110126@gmail.com>
2019-10-19 01:19:21 +0800

    complete tutorial episode 34

--- app-src/resources/views/welcome.blade.php
+++ app-src/resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <!-- Styles -->
         <style>
             html, body {
@@ -99,5 +99,6 @@
                 </div>
             </div>
         </div>
+        <script src="{{ mix('/js/app.js') }}"></script>
     </body>
 </html>
