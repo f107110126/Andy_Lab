0fcef5306b426202d635c563e872cddaaa0bd80e
Andy.Chen <f107110126@gmail.com>
2020-12-03 11:04:21 +0800

    step 002. Design the Timeline

---
+++ app-src/resources/views/_sidebar-links.blade.php
@@ -0,0 +1,10 @@
+<ul>
+    <li><a class="font-bold text-lg mb-4 block" href="{{ url('/') }}">Home</a></li>
+    <li><a class="font-bold text-lg mb-4 block" href="{{ url('/explore') }}">Explore</a></li>
+    <li><a class="font-bold text-lg mb-4 block" href="#">Notifications</a></li>
+    <li><a class="font-bold text-lg mb-4 block" href="#">Message</a></li>
+    <li><a class="font-bold text-lg mb-4 block" href="#">Bookmarks</a></li>
+    <li><a class="font-bold text-lg mb-4 block" href="#">Lists</a></li>
+    <li><a class="font-bold text-lg mb-4 block" href="#">Home</a></li>
+    <li><a class="font-bold text-lg mb-4 block" href="#">Home</a></li>
+</ul>
---
+++ app-src/resources/views/_tweet.blade.php
@@ -0,0 +1,11 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, sapiente vero. Unde eius
+            nemo praesentium suscipit? Quasi assumenda dolor, nihil molestias autem similique nisi ad,
+            laudantium pariatur, aspernatur nostrum praesentium?</p>
+    </div>
+</div>
